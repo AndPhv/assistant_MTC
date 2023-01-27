3326d9b8bdf3539c89e79c35fa8ff79be5c752b6
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2023-01-27 17:46:09 +0300

    rebased

--- app/src/main/java/com/example/assistant_mtc/screen/MainActivity.kt
+++ app/src/main/java/com/example/assistant_mtc/screen/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.assistant_mtc
+package com.example.assistant_mtc.screen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.assistant_mtc.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
