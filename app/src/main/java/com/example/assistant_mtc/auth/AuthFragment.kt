9a3ee1cd56a0a69658d2a299b2efa5e4263ac424
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2023-01-27 17:46:24 +0300

    added auth fragment classes

---
+++ app/src/main/java/com/example/assistant_mtc/auth/AuthFragment.kt
@@ -0,0 +1,8 @@
+package com.example.assistant_mtc.auth
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+
+class AuthFragment : Fragment() {
+    private val vm: AuthVM by viewModels()
+}
---
+++ app/src/main/java/com/example/assistant_mtc/auth/AuthVM.kt
@@ -0,0 +1,6 @@
+package com.example.assistant_mtc.auth
+
+import androidx.lifecycle.ViewModel
+
+class AuthVM : ViewModel() {
+}
