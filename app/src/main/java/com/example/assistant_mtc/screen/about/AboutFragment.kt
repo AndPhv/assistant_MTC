b1286ad65254d1e6d47857b0bb5593e3249d8dbd
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2023-01-28 12:54:59 +0300

    added about fragment and view model

---
+++ app/src/main/java/com/example/assistant_mtc/screen/about/AboutFragment.kt
@@ -0,0 +1,7 @@
+package com.example.assistant_mtc.screen.about
+
+import androidx.fragment.app.Fragment
+import com.example.assistant_mtc.R
+
+class AboutFragment : Fragment(R.layout.fragment_about) {
+}
---
+++ app/src/main/java/com/example/assistant_mtc/screen/about/AboutVM.kt
@@ -0,0 +1,6 @@
+package com.example.assistant_mtc.screen.about
+
+import androidx.lifecycle.ViewModel
+
+class AboutVM : ViewModel() {
+}
