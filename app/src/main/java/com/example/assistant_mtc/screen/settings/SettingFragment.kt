8e965ea8263de11e45dc112a079aaf9f9cac6814
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2023-01-28 19:05:29 +0300

    added settings info fragment and view model

---
+++ app/src/main/java/com/example/assistant_mtc/screen/settings/SettingFragment.kt
@@ -0,0 +1,6 @@
+package com.example.assistant_mtc.screen.settings
+
+import androidx.fragment.app.Fragment
+
+class SettingFragment: Fragment() {
+}
---
+++ app/src/main/java/com/example/assistant_mtc/screen/settings/SettingsVM.kt
@@ -0,0 +1,6 @@
+package com.example.assistant_mtc.screen.settings
+
+import androidx.lifecycle.ViewModel
+
+class SettingsVM : ViewModel() {
+}
