32bfd85ca30b4612c9d213b8b2e15c8122fd9690
Timofey <Ghost.Spartan36@gmail.com>
2023-03-15 11:55:49 +0300

    Переписан адаптер для работников

--- app/src/main/java/com/example/assistant_mtc/screen/employee/EmployeeAdapter.kt
+++ app/src/main/java/com/example/assistant_mtc/screen/employee/EmployeeAdapter.kt
@@ -1,32 +1,46 @@
 package com.example.assistant_mtc.screen.employee
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.assistant_mtc.databinding.EmployeeItemBinding
-//import kotlinx.android.synthetic.main.employee_item.view.*
+import com.example.assistant_mtc.R
+import com.sogya.mtc.domain.models.EmployeeDomain
 
-class EmployeeAdapter: RecyclerView.Adapter<EmployeeAdapter.EmployeeHolder>() {
+class EmployeeAdapter : RecyclerView.Adapter<EmployeeAdapter.EmployeeViewHolder>() {
 
-    private var employeeList = emptyList<com.sogya.mtc.domain.models.Employee>()
+    private var employeeList = arrayListOf<EmployeeDomain>()
 
-    inner class EmployeeHolder(val binding: EmployeeItemBinding): RecyclerView.ViewHolder(binding.root)
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EmployeeHolder {
-        return EmployeeHolder(EmployeeItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    //Вью холдер правильно создавать вот так
+    class EmployeeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val nameTextView: TextView = itemView.findViewById(R.id.textViewName)
+        val jobTextView: TextView = itemView.findViewById(R.id.textViewJob)
     }
 
-    override fun onBindViewHolder(holder: EmployeeHolder, position: Int) {
-        holder.binding.tvFullName.text = employeeList[position].fullName
-        holder.binding.tvTitleJob.text = employeeList[position].jobTitle
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EmployeeViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.employee_item, parent, false)
+        return EmployeeViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: EmployeeViewHolder, position: Int) {
+        val employee = employeeList[position]
+        holder.nameTextView.text = employee.fullName
+        holder.jobTextView.text = employee.jobTitle
     }
 
     override fun getItemCount(): Int {
         return employeeList.size
     }
 
-    fun setList(list: List<com.sogya.mtc.domain.models.Employee>) {
-        employeeList = list
-        notifyDataSetChanged()
+    //Данные в адаптер поставляются через этот метод.
+    //Сначала необходимо отчистить список, а потом уже вносить новые данные
+    fun updateEmployeeList(inputArrayList: List<EmployeeDomain>) {
+        this.employeeList.clear()
+        notifyItemChanged(1)
+        this.employeeList.addAll(inputArrayList)
+        notifyItemRangeChanged(0, inputArrayList.size)
     }
 }
