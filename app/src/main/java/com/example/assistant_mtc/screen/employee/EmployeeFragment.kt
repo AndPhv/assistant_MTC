fe43ecff049e72a0675617b5794491e8d35c263a
Timofey <Ghost.Spartan36@gmail.com>
2023-03-15 12:41:19 +0300

    Реализовал передачу списка в recycler view

--- app/src/main/java/com/example/assistant_mtc/screen/employee/EmployeeFragment.kt
+++ app/src/main/java/com/example/assistant_mtc/screen/employee/EmployeeFragment.kt
@@ -1,9 +1,43 @@
 package com.example.assistant_mtc.screen.employee
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.assistant_mtc.R
+import com.example.assistant_mtc.databinding.FragmentEmployeeBinding
 
 class EmployeeFragment : Fragment(R.layout.fragment_employee) {
-    private val vm:EmployeeVM by viewModels()
+    private lateinit var binding: FragmentEmployeeBinding
+    private val vm: EmployeeVM by viewModels()
+    private val employeeAdapter = EmployeeAdapter()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentEmployeeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        //Для получения данных из вью модели, подписываемся на нужную лайв дату в методе onResume()
+        vm.getEmployeeLiveData().observe(viewLifecycleOwner) {
+            //Обновляем данные в адаптере
+            employeeAdapter.updateEmployeeList(it)
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            rcView.adapter = employeeAdapter
+            rcView.layoutManager = LinearLayoutManager(requireContext())
+        }
+    }
 }
--- app/src/main/java/com/example/assistant_mtc/screen/employee/EmployeeVM.kt
+++ app/src/main/java/com/example/assistant_mtc/screen/employee/EmployeeVM.kt
@@ -1,6 +1,27 @@
 package com.example.assistant_mtc.screen.employee
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.sogya.mtc.domain.models.EmployeeDomain
 
 class EmployeeVM : ViewModel() {
+    //Для инициализации листа, достаточно напсиать так, отдельыне классы создавать не нужно
+    private val employeeList = arrayListOf(
+        EmployeeDomain("Каргапольцев Аркадий Аркадьевич", "111"),
+        EmployeeDomain("Ситников Виктор Валерьевич", "222"),
+        EmployeeDomain("Непорожнев Денис Александрович", "333"),
+        EmployeeDomain("Шубин Андрей Владимирович", "444"),
+        EmployeeDomain("Белоус Сергей Ильич", "555")
+    )
+
+    // Через лайв дату, данные передаются из вью модели на экран.
+    // Это позволяет выводить на экарн актуальные данные, ведь изменения отобразяться моментально
+    private val employeeLiveData = MutableLiveData<List<EmployeeDomain>>()
+
+    fun getEmployeeLiveData(): LiveData<List<EmployeeDomain>> = employeeLiveData
+
+    init {
+        employeeLiveData.value = employeeList
+    }
 }
