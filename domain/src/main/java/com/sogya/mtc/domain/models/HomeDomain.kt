9f363022214b620ef3cb80bb6ca4840c6ab7f21c
AndPhv <puhov2001@gmail.com>
2023-03-21 19:18:53 +0300

    Изменение раздела home, данные о расписании занятий теперь отображаются в виде списка recycleView

---
+++ domain/src/main/java/com/sogya/mtc/domain/models/HomeDomain.kt
@@ -0,0 +1,9 @@
+package com.sogya.mtc.domain.models
+
+data class HomeDomain(
+    val numLes: String,
+    val timeLes: String,
+    val lesson: String,
+    val employee: String,
+    val audience: String
+)
