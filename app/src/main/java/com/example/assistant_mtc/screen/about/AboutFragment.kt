8fb20dccd364129ddc2b0485ea1a7bc850fd8304
Timofey <Ghost.Spartan36@gmail.com>
2023-03-21 17:06:38 +0300

    добавил рализацию интерфейса нажатия на элементы из адаптера

--- app/src/main/java/com/example/assistant_mtc/screen/about/AboutFragment.kt
+++ app/src/main/java/com/example/assistant_mtc/screen/about/AboutFragment.kt
@@ -1,19 +1,22 @@
 package com.example.assistant_mtc.screen.about
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.assistant_mtc.R
 import com.example.assistant_mtc.databinding.FragmentAboutBinding
+import com.sogya.mtc.domain.utils.CallbackListener
 
-class AboutFragment : Fragment(R.layout.fragment_about) {
+class AboutFragment : Fragment(R.layout.fragment_about), AboutAdapter.AboutOnClickListener {
     private lateinit var binding: FragmentAboutBinding
-    private val vm:AboutVM by viewModels()
-    private val aboutAdapter = AboutAdapter()
+    private val vm: AboutVM by viewModels()
+    private val aboutAdapter = AboutAdapter(this)
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,5 +40,17 @@ class AboutFragment : Fragment(R.layout.fragment_about) {
             rcView.adapter = aboutAdapter
             rcView.layoutManager = LinearLayoutManager(requireContext())
         }
+    }
+
+    override fun onClick(command: String) {
+        vm.openTelegramByCommand(command, object : CallbackListener<Intent> {
+            override fun data(data: Intent) {
+                startActivity(data)
+            }
+
+            override fun error(error: String) {
+                Toast.makeText(requireContext(), error, Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
