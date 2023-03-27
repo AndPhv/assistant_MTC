50ab923f9b30211e58db5f8878f3c571c001cf41
AndPhv <puhov2001@gmail.com>
2023-03-27 18:25:15 +0300

    Изменение зависимостей Room и добавление таблицы в LessonDomain.kt

--- domain/src/main/java/com/sogya/mtc/domain/models/LessonDomain.kt
+++ domain/src/main/java/com/sogya/mtc/domain/models/LessonDomain.kt
@@ -1,8 +1,27 @@
 package com.sogya.mtc.domain.models
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.sogya.mtc.domain.models.LessonDomain.Companion.TABLE_NAME
+
+@Entity(
+    tableName = TABLE_NAME
+)
+
 data class LessonDomain(
+    @PrimaryKey
+    val id: String,
     val audience: String,
     val lesson: String,
     val employee: String,
     val infoLesson: String
-)
+) {
+    companion object {
+        const val TABLE_NAME = "LessonInfo"
+        const val ID = "Id"
+        const val AUDIENCE = "Audience"
+        const val LESSON = "Lesson"
+        const val EMPLOYEE = "Employee"
+        const val INFOLESSON = "InfoLesson"
+    }
+}
