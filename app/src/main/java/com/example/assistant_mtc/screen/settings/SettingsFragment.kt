023a137ac68a49ebc1b5f495a40ff807e6b8f289
Timofey <Ghost.Spartan36@gmail.com>
2023-03-29 20:20:55 +0300

    добавил экарн настроек

--- app/src/main/java/com/example/assistant_mtc/screen/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/assistant_mtc/screen/settings/SettingsFragment.kt
@@ -1,6 +1,32 @@
 package com.example.assistant_mtc.screen.settings
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.assistant_mtc.R
+import com.example.assistant_mtc.databinding.FragmentSettingsBinding
 
-class SettingsFragment: Fragment() {
+class SettingsFragment : Fragment(R.layout.fragment_settings) {
+    private val vm:SettingsVM by viewModels()
+    private lateinit var binding: FragmentSettingsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSettingsBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.textViewHelp.setOnClickListener{
+            Toast.makeText(requireContext(), "В процессе...", Toast.LENGTH_SHORT).show()
+        }
+    }
 }
