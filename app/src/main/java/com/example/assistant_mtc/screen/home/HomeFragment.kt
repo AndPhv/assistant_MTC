f4b411beafd265c6fd3c7e90d059a8e6b194ac0c
Timofey <Ghost.Spartan36@gmail.com>
2023-03-29 20:21:32 +0300

    внес исправления в навигацию

--- app/src/main/java/com/example/assistant_mtc/screen/home/HomeFragment.kt
+++ app/src/main/java/com/example/assistant_mtc/screen/home/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.assistant_mtc.R
 import com.example.assistant_mtc.databinding.FragmentHomeBinding
