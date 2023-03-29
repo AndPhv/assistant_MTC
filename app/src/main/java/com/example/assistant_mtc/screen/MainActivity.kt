d69cb0dc8e33fb3932219eee760ea4c605531664
Timofey <Ghost.Spartan36@gmail.com>
2023-03-29 20:23:13 +0300

    настроил переход в настройки

--- app/src/main/java/com/example/assistant_mtc/screen/MainActivity.kt
+++ app/src/main/java/com/example/assistant_mtc/screen/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.assistant_mtc.screen
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -34,6 +35,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.appbar_menu, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.settingsFragment -> {
+                navController.navigate(R.id.settingsFragment)
+                true
+            }
+            else->{
+                super.onOptionsItemSelected(item)
+            }
+        }
     }
 
     @Deprecated("Deprecated in Java")
