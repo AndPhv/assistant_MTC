c7c9549409d5581e7ebaab2166b6e34c47e4bfa0
SogYa <Ghost.Spartan36@gmail.com>
2023-05-22 13:10:24 +0300

    удалены ненужные фрагменты

--- app/src/main/java/com/example/assistant_mtc/screen/groupinfo/GroupInfoFragment.kt
+++
@@ -1,6 +0,0 @@
-package com.example.assistant_mtc.screen.groupinfo
-
-import androidx.fragment.app.Fragment
-
-class GroupInfoFragment : Fragment() {
-}
--- app/src/main/java/com/example/assistant_mtc/screen/groupinfo/GroupInfoVM.kt
+++
@@ -1,6 +0,0 @@
-package com.example.assistant_mtc.screen.groupinfo
-
-import androidx.lifecycle.ViewModel
-
-class GroupInfoVM : ViewModel() {
-}
