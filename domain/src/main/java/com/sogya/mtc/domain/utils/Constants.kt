6cb729406935e58519748d33de7794dce540facb
SogYa <Ghost.Spartan36@gmail.com>
2023-06-01 15:12:30 +0300

    изменен тестовый адрес

--- domain/src/main/java/com/sogya/mtc/domain/utils/Constants.kt
+++ domain/src/main/java/com/sogya/mtc/domain/utils/Constants.kt
@@ -4,6 +4,6 @@ object Constants {
     const val OPEN_TG_NEWS = "tgNews"
     const val OPEN_TG_BOT = "tgBot"
     const val OPEN_ADDRESS = "address"
-    const val BASE_URI = "baseUri"
+    const val BASE_URI = "https://127.0.0.1"
     const val BASE_TOKEN = "token"
 }
